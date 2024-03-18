4ded6468ba3dc5b2a778c7ca623a4e348ace6749
KaiKaiKay <candy168741@gmail.com>
2024-03-18 10:16:33 +0800

    add 原始寫法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TodoService, Todo } from './todo.service';
+// import { TodoService, Todo } from './todo.service';
 
 export class StoreModule {}
+
+interface Todo {
+  text: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,25 +18,46 @@ export class StoreModule {}
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  // 原始寫法
+  todos: Todo[] = [];
   newTodo: string = '';
 
-  constructor(private todoService: TodoService) {}
-
-  /** 取得 service 裡的代辦事項表 */
-  get todos() {
-    return this.todoService.getTodos();
-  }
-
   addTodo() {
-    this.todoService.addTodo(this.newTodo);
-    this.newTodo = ''; //清空
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ text: this.newTodo, completed: false });
+      this.newTodo = '';
+    }
   }
 
   toggleCompleted(todo: Todo) {
-    this.todoService.toggleCompleted(todo);
+    todo.completed = !todo.completed;
   }
 
   deleteTodo(todo: Todo) {
-    this.todoService.deleteTodo(todo);
+    this.todos = this.todos.filter(t => t !== todo);
   }
+
+
+  //使用 service 寫法
+  // newTodo: string = '';
+
+  // constructor(private todoService: TodoService) {}
+
+  // /** 取得 service 裡的代辦事項表 */
+  // get todos() {
+  //   return this.todoService.getTodos();
+  // }
+
+  // addTodo() {
+  //   this.todoService.addTodo(this.newTodo);
+  //   this.newTodo = ''; //清空
+  // }
+
+  // toggleCompleted(todo: Todo) {
+  //   this.todoService.toggleCompleted(todo);
+  // }
+
+  // deleteTodo(todo: Todo) {
+  //   this.todoService.deleteTodo(todo);
+  // }
 }
